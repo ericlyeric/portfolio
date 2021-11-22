eced3242566965872ca9b967e1459d3fef8f9787
Eric <ericlyly@hotmail.com>
2021-11-21 16:26:02 -0800

    add files

---
+++ webpack/webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['./src/scripts/game.js'],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          filename: '[name].bundle.js'
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ name: 'Eric Ly | portfolio', template: 'src/index.html' }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'assets' },
+      { from: 'src/favicon.ico', to: '' }
+    ]),
+  ]
+}
